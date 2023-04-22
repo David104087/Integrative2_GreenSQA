6de2216bbc6f780a55f0f71139bdacadc097497b
David104087 <1040871155@u.icesi.edu.co>
2023-04-22 17:02:47 -0500

    Upload all

--- doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"model","l":"Capsule"},{"p":"model","l":"Collaborator"},{"p":"model","l":"KnowledgeCapsuleSystem"},{"p":"ui","l":"Main"},{"p":"model","l":"Manager"},{"p":"model","l":"Project"},{"p":"model","l":"Stage"}];updateSearchResults();
